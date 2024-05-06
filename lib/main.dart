fd3ffcba301d4fd8fe5fea26474317e53a3f3c2b
Pawan Bhayde <pawanbhayde721@gmail.com>
2024-05-06 22:52:17 +0530

    Add wallpapers and update homepage layout

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,10 @@ import 'package:wallapix/pages/onboarding.dart';
 
 void main() {
   runApp(const MyApp());
-}
+} 
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  const MyApp({super.key});    
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -17,7 +15,7 @@ class _HomePageState extends State<HomePage> {
         leading: IconButton(
           icon: const Icon(Icons.menu),
           onPressed: () {},
-        ),     
+        ),
         title: const Text('Wallapix',
             style: TextStyle(
               fontSize: 16,
@@ -31,8 +29,84 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      body: const Center(
-        child: Text('Home Page'),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 60,
+              width: 60,
+              decoration: BoxDecoration(
+                color: Colors.deepPurple.shade50,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child:
+                  const Center(child: Text('1250', style: TextStyle(fontSize: 18))),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'New Release',
+              style: TextStyle(fontSize: 24),
+            ),
+            const Text('Give your screen a makeover!'),
+            const SizedBox(height: 10),
+            // Catergory chips for All, Free, Pro, and filter icon
+            Wrap(
+              spacing: 10,
+              children: [
+                Chip(
+                  side: BorderSide.none,
+                  label: const Text('All'),
+                  backgroundColor: Colors.deepPurple.shade50,
+                ),
+                Chip(
+                  side: BorderSide.none,
+                  label: const Text('Free'),
+                  backgroundColor: Colors.deepPurple.shade50,
+                ),
+                Chip(
+                  side: BorderSide.none,
+                  label: const Text('Pro'),
+                  backgroundColor: Colors.deepPurple.shade50,
+                ),
+                Chip(
+                  side: BorderSide.none,
+                  label: const Icon(Icons.filter_alt_outlined),
+                  backgroundColor: Colors.deepPurple.shade50,
+                ),
+              ],
+            ),
+            const SizedBox(height: 30),
+
+            // Scrollable GridView for images in 2 columns
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: GridView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                ),
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple.shade50,
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                        image:
+                            AssetImage('assets/images/wallpapers${index + 1}.jpg'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
       // floatingActionButton with text
       floatingActionButton: FloatingActionButton.extended(
